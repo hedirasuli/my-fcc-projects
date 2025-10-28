432c3b2d80f786824747b06f54e7fa53f31b766b
hedirasuli <hedi.rasuli21@gmail.com>
2025-10-28 14:40:13 +0100

    feat: Complete the JavaScript Code Bot project

---
+++ Build a JavaScript Trivia Bot/Build a JavaScript Trivia Bot.js
@@ -0,0 +1,20 @@
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+let  botName = "CodeBot";
+let  botLocation = "the internet";
+let  favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName +" and I live on "+ botLocation + ".")
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Did you know that "+ favoriteLanguage + " was created in just 10 days by Brendan Eich in 1995?";
+console.log(codingFact);
+
+codingFact = "Here's another fun fact: " + favoriteLanguage +  "The first computer bug was an actual moth found in a computer in 1947!";
+console.log(codingFact);
+
+codingFact = "Another fact: " + favoriteLanguage + " is often used for both front-end and back-end development, making it a versatile language!";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - "+botName+" from "+botLocation+".");
