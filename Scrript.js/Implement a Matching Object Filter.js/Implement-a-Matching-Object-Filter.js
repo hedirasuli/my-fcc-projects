627d6ec5d56027848c1f48e56cf4037c9a9a9ab4
hedirasuli <hedi.rasuli21@gmail.com>
2025-11-13 18:23:08 +0100

    feat: Implement object property matcher/filter

---
+++ Scrript.js/Implement a Matching Object Filter.js/Implement-a-Matching-Object-Filter.js
@@ -0,0 +1,10 @@
+function whatIsInAName(collection, source) {
+ 
+  const sourceKeys = Object.keys(source);
+
+  
+  return collection.filter(obj => {
+   
+    return sourceKeys.every(key => obj.hasOwnProperty(key) && obj[key] === source[key]);
+  });
+}
