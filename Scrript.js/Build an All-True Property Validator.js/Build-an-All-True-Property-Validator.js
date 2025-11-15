c559f9d42700c7f375fee75660cfb0d056532e63
hedirasuli <hedi.rasuli21@gmail.com>
2025-11-15 15:20:46 +0100

    feat: Implement truthCheck for all-true property validation
    Adds a function that checks if a specified property has a truthy value in every object within a collection array.

---
+++ Scrript.js/Build an All-True Property Validator.js/Build-an-All-True-Property-Validator.js
@@ -0,0 +1,10 @@
+function truthCheck(collection, pre) {
+  
+  return collection.every(function(obj) {
+    
+    return obj[pre];
+  });
+}
+
+truthCheck([{name: "Quincy", role: "Founder", isBot: false},
+ {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot");
