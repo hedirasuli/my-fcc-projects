1bfef98c5b031bc46bc9cae9e47971c37f3f5a98
hedirasuli <hedi.rasuli21@gmail.com>
2026-06-28 21:41:08 +0200

    Update documentation for truthCheck function to clarify parameters and return value

--- javascript-projects/all-true Property-validator/Build-an-All-True-Property-Validator.js
+++ javascript-projects/all-true Property-validator/Build-an-All-True-Property-Validator.js
@@ -1,3 +1,13 @@
+/**
+ * truthCheck - Checks if a given property (pre) is truthy for every object in a collection.
+ * 
+ * @param {Array} collection - An array of objects to check.
+ * @param {string} pre - The property key to evaluate on each object.
+ * @returns {boolean} - Returns true if the property is truthy for all objects, false otherwise.
+ * 
+ * The every() method tests whether all elements in the array pass the test implemented
+ * by the provided function. Here, each object's 'pre' property is evaluated for truthiness.
+ */
 function truthCheck(collection, pre) {
   
   return collection.every(function(obj) {
