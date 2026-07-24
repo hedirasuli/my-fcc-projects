57d5bda24cce86fceeafeb709dcf7678dd4fa97d
hedirasuli <hedi.rasuli21@gmail.com>
2026-07-24 20:47:07 +0200

    Update documentation for pairElement function with detailed parameter and return descriptions

--- javascript-projects/dna-pair-generator/dna-pair-generator.js
+++ javascript-projects/dna-pair-generator/dna-pair-generator.js
@@ -1,7 +1,22 @@
 /**
- 
- * @param {string} str 
- * @returns {string[][]} 
+ * pairElement - Returns an array of DNA base pairs for a given DNA strand.
+ * 
+ * DNA base pairing rules:
+ *   - A (Adenine) pairs with T (Thymine)
+ *   - T (Thymine) pairs with A (Adenine)
+ *   - C (Cytosine) pairs with G (Guanine)
+ *   - G (Guanine) pairs with C (Cytosine)
+ * 
+ * @param {string} str - A string containing DNA bases (e.g., "ATCGA")
+ * @returns {Array<Array<string>>} - A 2D array where each inner array is a pair [original, complement]
+ * 
+ * @example
+ * // returns [["A", "T"], ["T", "A"], ["C", "G"], ["G", "C"], ["A", "T"]]
+ * pairElement("ATCGA");
+ * 
+ * @example
+ * // returns [["G", "C"], ["C", "G"], ["G", "C"]]
+ * pairElement("GCG");
  */
 function pairElement(str) {
 
