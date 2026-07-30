3521ecaebb5a1df56f3920bc9d18af2f77a24210
hedirasuli <hedi.rasuli21@gmail.com>
2026-07-30 20:11:01 +0200

    Improve JSDoc comments for clarity in html-entity-converter.js

--- javascript-projects/html-entity-converter/html-entity-converter.js
+++ javascript-projects/html-entity-converter/html-entity-converter.js
@@ -1,10 +1,10 @@
 /**
- * @param {string} str - رشته ورودی حاوی کاراکترهای خاص.
- * @returns {string} رشته جدید با Entityهای HTML.
+ * Convert special characters in a string to their HTML entities.
+ * @param {string} str - String containing special characters
+ * @returns {string} - New string with HTML entities
  */
-
 function convertHTML(str) {
-    
+    // Mapping of special characters to their HTML entity equivalents
     const htmlEntities = {
         "&": "&amp;",
         "<": "&lt;",
@@ -13,6 +13,6 @@ function convertHTML(str) {
         "'": "&apos;",
     };
 
-   
+    // Replace all occurrences of &, <, >, ", ' with their HTML entities
     return str.replace(/[&<>"']/g, (match) => htmlEntities[match]);
 }
