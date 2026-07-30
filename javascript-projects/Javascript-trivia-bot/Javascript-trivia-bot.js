b2d1f344a788a4113bf81be5d2d8ef5c972fa80b
hedirasuli <hedi.rasuli21@gmail.com>
2026-07-30 20:17:56 +0200

    Improve comments for clarity in Javascript-trivia-bot.js

--- javascript-projects/Javascript-trivia-bot/Javascript-trivia-bot.js
+++ javascript-projects/Javascript-trivia-bot/Javascript-trivia-bot.js
@@ -1,20 +1,20 @@
-
+// Welcome message
 console.log("Hello! I'm your coding fun fact guide!");
-
+// Bot details
 let  botName = "CodeBot";
 let  botLocation = "the internet";
 let  favoriteLanguage = "JavaScript";
-
+// Introduction
 console.log("My name is " + botName +" and I live on "+ botLocation + ".")
 console.log("My favorite programming language is " + favoriteLanguage + ".");
-
+// Fun fact #1
 let codingFact = "Did you know that "+ favoriteLanguage + " was created in just 10 days by Brendan Eich in 1995?";
 console.log(codingFact);
-
+// Fun fact #2
 codingFact = "Here's another fun fact: " + favoriteLanguage +  "The first computer bug was an actual moth found in a computer in 1947!";
 console.log(codingFact);
-
+// Fun fact #3
 codingFact = "Another fact: " + favoriteLanguage + " is often used for both front-end and back-end development, making it a versatile language!";
 console.log(codingFact);
-
+// Goodbye message
 console.log("It was fun sharing these facts with you. Goodbye! - "+botName+" from "+botLocation+".");
