dc621ad05e02b57f786f76c619e0b4abe7b743a6
hedirasuli <hedi.rasuli21@gmail.com>
2026-07-31 20:23:28 +0200

    Improve comments for clarity in largest-number-finder.js

--- javascript-projects/largest-number-finder/largest-number-finder.js
+++ javascript-projects/largest-number-finder/largest-number-finder.js
@@ -1,24 +1,29 @@
+/**
+ * Find the largest number in each sub-array.
+ * @param {number[][]} arr - 2D array of numbers
+ * @returns {number[]} - Array containing the largest number from each sub-array
+ */
 function largestOfAll(arr) {
      const largestNumbers = [];
 
-   
+     // Loop through each sub-array
     for (let i = 0; i < arr.length; i++) {
   
           const subArray = arr[i];
 
-        
+        // Start with the first element as the largest
         let largestNumInSub = subArray[0];
 
       
-     
+        // Loop through the rest of the sub-array
         for (let j = 1; j < subArray.length; j++) {
 
-         
+            // Update largest if current element is bigger
             if (subArray[j] > largestNumInSub) {
                 largestNumInSub = subArray[j];
             }
         }
-       
+        // Add the largest number to result array
         largestNumbers.push(largestNumInSub);
     }
    
